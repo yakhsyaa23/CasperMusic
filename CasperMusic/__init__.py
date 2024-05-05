91d89b00263acde6537eca8b663b465d9cda47f9
CasperCloudku <casper@cloudku.site>
2024-05-05 18:36:55 +0700

    CasperMusic

---
+++ CasperMusic/__init__.py
@@ -0,0 +1,26 @@
+from CasperMusic.core.bot import Anony
+from CasperMusic.core.dir import dirr
+from CasperMusic.core.git import git
+from CasperMusic.core.userbot import Userbot
+from CasperMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Anony()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
